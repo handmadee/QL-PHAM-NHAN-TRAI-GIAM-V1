18bf9524929dcb6c07e3bc5920570018c9fee699
Dino_datcoder38 <dinodatcoder38@gmail.com>
2024-10-20 11:38:37 +0700

    init project

---
+++ src/main/java/com/mycompany/ql/pham/nhan/trai/giam/v1/QLPHAMNHANTRAIGIAMV1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ql.pham.nhan.trai.giam.v1;
+
+/**
+ *
+ * @author admin
+ */
+public class QLPHAMNHANTRAIGIAMV1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
